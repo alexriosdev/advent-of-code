704f2964233f90f052d0b17efa042e8b2ef09e50
Alex Rios <alexrios.dev@gmail.com>
2023-12-02 19:48:34 -0600

    complete day 1 part 2

--- 2023/day01/main.go
+++ 2023/day01/main.go
@@ -10,6 +10,7 @@ func main() {
 	lines, _ := utils.ReadLines("input.txt")
 	fmt.Println("2023 Day 01 Solution")
 	fmt.Printf("Part 1: %v\n", part1(lines))
+	fmt.Printf("Part 2: %v\n", part2(lines))
 }
 
 func part1(lines []string) int {
@@ -18,13 +19,57 @@ func part1(lines []string) int {
 		first, last := 0, 0
 		for _, c := range s {
 			if unicode.IsDigit(c) {
-				if first == 0 {
-					first = int(c - '0')
-				}
-				last = int(c - '0')
+				first, last = updateDigits(first, last, int(c-'0'))
 			}
 		}
 		result += (first * 10) + last
 	}
 	return result
+}
+
+func part2(lines []string) int {
+	result := 0
+	numberMap := map[string]int{
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+	}
+	for _, s := range lines {
+		first, last := 0, 0
+		for i, c := range s {
+			n := len(s)
+			if unicode.IsDigit(c) {
+				first, last = updateDigits(first, last, int(c-'0'))
+				continue
+			}
+			if i < n-2 && numberMap[s[i:i+3]] != 0 {
+				first, last = updateDigits(first, last, numberMap[s[i:i+3]])
+				continue
+			}
+			if i < n-3 && numberMap[s[i:i+4]] != 0 {
+				first, last = updateDigits(first, last, numberMap[s[i:i+4]])
+				continue
+			}
+			if i < n-4 && numberMap[s[i:i+5]] != 0 {
+				first, last = updateDigits(first, last, numberMap[s[i:i+5]])
+				continue
+			}
+		}
+		result += (first * 10) + last
+	}
+	return result
+}
+
+func updateDigits(first, last, val int) (int, int) {
+	if first == 0 {
+		first = val
+	}
+	last = val
+	return first, last
 }
--- 2023/day01/main_test.go
+++ 2023/day01/main_test.go
@@ -22,3 +22,21 @@ func TestPart1(t *testing.T) {
 		}
 	}
 }
+
+func TestPart2(t *testing.T) {
+	lines1, _ := utils.ReadLines("input_test2.txt")
+	lines2, _ := utils.ReadLines("input.txt")
+	tests := []struct {
+		input    []string
+		expected int
+	}{
+		{lines1, 281},
+		{lines2, 53348},
+	}
+	for _, test := range tests {
+		result := part2(test.input)
+		if result != test.expected {
+			t.Errorf("Result %v not equal to expected %v", result, test.expected)
+		}
+	}
+}
