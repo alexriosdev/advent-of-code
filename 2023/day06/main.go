362972c550a861d1122e37db109fe19a79d216bf
Alex Rios <alexrios.dev@gmail.com>
2023-12-06 00:54:44 -0600

    complete day 6 part 2

--- 2023/day06/main.go
+++ 2023/day06/main.go
@@ -11,6 +11,7 @@ func main() {
 	lines, _ := utils.ReadLines("input.txt")
 	fmt.Println("2023 Day 06 Solution")
 	fmt.Printf("Part 1: %v\n", part1(lines))
+	fmt.Printf("Part 2: %v\n", part2(lines))
 }
 
 func part1(lines []string) int {
@@ -31,6 +32,30 @@ func part1(lines []string) int {
 		}
 	}
 	return result
+}
+
+func part2(lines []string) int {
+	result := 1
+	count := 0
+	time, dist, availableDist := buildNumber(lines[0]), buildNumber(lines[1]), buildNumber(lines[0])
+	for currTime := 0; currTime < time; currTime++ {
+		if currTime*availableDist > dist {
+			count++
+		}
+		availableDist--
+	}
+	if count != 0 {
+		result *= count
+	}
+	return result
+}
+
+func buildNumber(line string) int {
+	sb := strings.Builder{}
+	for _, s := range strings.Fields(strings.Split(line, ":")[1]) {
+		sb.WriteString(s)
+	}
+	return strToInt(sb.String())
 }
 
 func strToInt(s string) int {
