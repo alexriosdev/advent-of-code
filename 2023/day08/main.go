513ce5fe55e7d889359178eb8e9c69b5ce05c0b1
Alex Rios <alexrios.dev@gmail.com>
2023-12-08 20:34:16 -0600

    complete day 8 part 2

--- 2023/day08/main.go
+++ 2023/day08/main.go
@@ -14,6 +14,7 @@ func main() {
 	lines, _ := utils.ReadLines("input.txt")
 	fmt.Println("2023 Day 08 Solution")
 	fmt.Printf("Part 1: %v\n", part1(lines))
+	fmt.Printf("Part 2: %v\n", part2(lines))
 }
 
 func part1(lines []string) int {
@@ -37,4 +38,49 @@ func part1(lines []string) int {
 		count++
 	}
 	return count
+}
+
+func part2(lines []string) int {
+	inst := []rune(lines[0])
+	replacer := strings.NewReplacer("=", "", "(", "", ",", "", ")", "")
+	startNodes := []string{}
+	nodeMap := map[string]node{}
+	for i := 1; i < len(lines); i++ {
+		split := strings.Fields(replacer.Replace(lines[i]))
+		nodeMap[split[0]] = node{left: split[1], right: split[2]}
+		if split[0][2] == 'A' {
+			startNodes = append(startNodes, split[0])
+		}
+	}
+	steps := []int{}
+	for _, curr := range startNodes {
+		count := 0
+		for curr[2] != 'Z' {
+			c := inst[count%len(inst)]
+			node := nodeMap[curr]
+			if c == 'L' {
+				curr = node.left
+			} else {
+				curr = node.right
+			}
+			count++
+		}
+		steps = append(steps, count)
+	}
+	return lcm(steps, 0)
+}
+
+func lcm(nums []int, i int) int {
+	if i == len(nums)-1 {
+		return nums[i]
+	}
+	a, b := nums[i], lcm(nums, i+1)
+	return (a * b) / gcm(a, b)
+}
+
+func gcm(a, b int) int {
+	if b == 0 {
+		return a
+	}
+	return gcm(b, a%b)
 }
--- 2023/day08/main_test.go
+++ 2023/day08/main_test.go
@@ -25,9 +25,34 @@ func TestPart1(t *testing.T) {
 	}
 }
 
+func TestPart2(t *testing.T) {
+	lines1, _ := utils.ReadLines("input_test3.txt")
+	lines2, _ := utils.ReadLines("input.txt")
+	tests := []struct {
+		input    []string
+		expected int
+	}{
+		{lines1, 6},
+		{lines2, 21366921060721},
+	}
+	for _, test := range tests {
+		result := part2(test.input)
+		if result != test.expected {
+			t.Errorf("Result %v not equal to expected %v", result, test.expected)
+		}
+	}
+}
+
 func BenchmarkPart1(b *testing.B) {
 	lines, _ := utils.ReadLines("input.txt")
 	for n := 0; n < b.N; n++ {
 		part1(lines)
+	}
+}
+
+func BenchmarkPart2(b *testing.B) {
+	lines, _ := utils.ReadLines("input.txt")
+	for n := 0; n < b.N; n++ {
+		part2(lines)
 	}
 }
