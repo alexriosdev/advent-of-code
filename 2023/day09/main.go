396198b489239748f8a715d42f372e214ae0c6e7
Alex Rios <alexrios.dev@gmail.com>
2023-12-09 22:39:15 -0600

    complete day 9 part 2

--- 2023/day09/main.go
+++ 2023/day09/main.go
@@ -11,6 +11,7 @@ func main() {
 	lines, _ := utils.ReadLines("input.txt")
 	fmt.Println("2023 Day 09 Solution")
 	fmt.Printf("Part 1: %v\n", part1(lines))
+	fmt.Printf("Part 2: %v\n", part2(lines))
 }
 
 func part1(lines []string) int {
@@ -23,6 +24,26 @@ func part1(lines []string) int {
 		result += computeLast(&predictions)
 	}
 	return result
+}
+
+func part2(lines []string) int {
+	result := 0
+	for _, line := range lines {
+		nums := lineToNumbers(line)
+		predictions := [][]int{}
+		predictions = append(predictions, nums)
+		computePredictions(&nums, &predictions)
+		result += computeFirst(&predictions)
+	}
+	return result
+}
+
+func computeFirst(predictions *[][]int) int {
+	first := 0
+	for i := len(*predictions) - 1; i >= 0; i-- {
+		first = (*predictions)[i][0] - first
+	}
+	return first
 }
 
 func computeLast(predictions *[][]int) int {
