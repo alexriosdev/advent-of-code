231a32b5dc0c19848925383e9c62557e183a8eb7
Alex Rios <alexrios.dev@gmail.com>
2023-12-13 20:18:42 -0600

    complete day 12 part 2

--- 2023/day12/main.go
+++ 2023/day12/main.go
@@ -11,12 +11,25 @@ func main() {
 	lines, _ := utils.ReadLines("input.txt")
 	fmt.Println("2023 Day 12 Solution")
 	fmt.Printf("Part 1: %v\n", part1(lines))
+	fmt.Printf("Part 2: %v\n", part2(lines))
 }
 
 func part1(lines []string) int {
 	sum := 0
 	for _, line := range lines {
 		split := strings.Split(line, " ")
+		runes, nums := []rune(split[0]), strToNumbers(split[1])
+		cache := map[[3]int]int{}
+		sum += arrangementCount(cache, runes, nums, 0, 0, 0)
+	}
+	return sum
+}
+
+func part2(lines []string) int {
+	sum := 0
+	for _, line := range lines {
+		split := strings.Split(line, " ")
+		split[0], split[1] = unfoldStrings(split[0], split[1])
 		runes, nums := []rune(split[0]), strToNumbers(split[1])
 		cache := map[[3]int]int{}
 		sum += arrangementCount(cache, runes, nums, 0, 0, 0)
@@ -64,6 +77,19 @@ func arrangementCount(cache map[[3]int]int, runes []rune, nums []int, i, j, num 
 		return count
 	}
 	return 0
+}
+
+func unfoldStrings(s1, s2 string) (string, string) {
+	sb1, sb2 := strings.Builder{}, strings.Builder{}
+	for i := 0; i < 4; i++ {
+		sb1.WriteString(s1)
+		sb1.WriteRune('?')
+		sb2.WriteString(s2)
+		sb2.WriteRune(',')
+	}
+	sb1.WriteString(s1)
+	sb2.WriteString(s2)
+	return sb1.String(), sb2.String()
 }
 
 func strToNumbers(s string) []int {
