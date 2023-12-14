475192babc863e61eeb5d7d9426f1033324fe399
Alex Rios <alexrios.dev@gmail.com>
2023-12-14 02:09:53 -0600

    complete day 13 part 2

--- 2023/day13/main.go
+++ 2023/day13/main.go
@@ -10,6 +10,7 @@ func main() {
 	input, _ := os.ReadFile("input.txt")
 	fmt.Println("2023 Day 13 Solution")
 	fmt.Printf("Part 1: %v\n", part1(input))
+	fmt.Printf("Part 2: %v\n", part2(input))
 }
 
 func part1(input []byte) int {
@@ -18,18 +19,31 @@ func part1(input []byte) int {
 		lines := strings.Split(inputSplit, "\n")
 		grid := linesToGrid(lines)
 		rows, cols := len(grid), len(grid[0])
-		result += getVerticalSum(grid, rows, cols)
-		result += getHorizontalSum(grid, rows, cols)
+		result += getVerticalSum(grid, rows, cols, false)
+		result += getHorizontalSum(grid, rows, cols, false)
 	}
 	return result
 }
 
-func getVerticalSum(grid [][]rune, rows, cols int) int {
+func part2(input []byte) int {
+	result := 0
+	for _, inputSplit := range strings.Split(strings.TrimSpace(string(input)), "\n\n") {
+		lines := strings.Split(inputSplit, "\n")
+		grid := linesToGrid(lines)
+		rows, cols := len(grid), len(grid[0])
+		result += getVerticalSum(grid, rows, cols, true)
+		result += getHorizontalSum(grid, rows, cols, true)
+	}
+	return result
+}
+
+func getVerticalSum(grid [][]rune, rows, cols int, hasSmudge bool) int {
 	sum := 0
 	for i := 0; i < cols-1; i++ {
 		isVertical := true
+		smudge := 0
 		for j := 0; j < cols; j++ {
-			if !isVertical {
+			if !isVertical && !hasSmudge {
 				break
 			}
 			left := i - j
@@ -40,23 +54,27 @@ func getVerticalSum(grid [][]rune, rows, cols int) int {
 			for k := 0; k < rows; k++ {
 				if grid[k][left] != grid[k][right] {
 					isVertical = false
+					smudge++
+				}
+				if !isVertical && !hasSmudge {
 					break
 				}
 			}
 		}
-		if isVertical {
+		if (isVertical && !hasSmudge) || (hasSmudge && smudge == 1) {
 			sum += i + 1
 		}
 	}
 	return sum
 }
 
-func getHorizontalSum(grid [][]rune, rows, cols int) int {
+func getHorizontalSum(grid [][]rune, rows, cols int, hasSmudge bool) int {
 	sum := 0
 	for i := 0; i < rows-1; i++ {
 		isHorizontal := true
+		smudge := 0
 		for j := 0; j < rows; j++ {
-			if !isHorizontal {
+			if !isHorizontal && !hasSmudge {
 				break
 			}
 			up := i - j
@@ -67,11 +85,14 @@ func getHorizontalSum(grid [][]rune, rows, cols int) int {
 			for k := 0; k < cols; k++ {
 				if grid[up][k] != grid[down][k] {
 					isHorizontal = false
+					smudge++
+				}
+				if !isHorizontal && !hasSmudge {
 					break
 				}
 			}
 		}
-		if isHorizontal {
+		if (isHorizontal && !hasSmudge) || (hasSmudge && smudge == 1) {
 			sum += (i + 1) * 100
 		}
 	}
--- 2023/day13/main_test.go
+++ 2023/day13/main_test.go
@@ -23,7 +23,32 @@ func TestPart1(t *testing.T) {
 	}
 }
 
+func TestPart2(t *testing.T) {
+	input1, _ := os.ReadFile("input_test.txt")
+	input2, _ := os.ReadFile("input.txt")
+	tests := []struct {
+		input    []byte
+		expected int
+	}{
+		{input1, 400},
+		{input2, 31954},
+	}
+	for _, test := range tests {
+		result := part2(test.input)
+		if result != test.expected {
+			t.Errorf("Result %v not equal to expected %v", result, test.expected)
+		}
+	}
+}
+
 func BenchmarkPart1(b *testing.B) {
+	input, _ := os.ReadFile("input_test.txt")
+	for n := 0; n < b.N; n++ {
+		part1(input)
+	}
+}
+
+func BenchmarkPart2(b *testing.B) {
 	input, _ := os.ReadFile("input_test.txt")
 	for n := 0; n < b.N; n++ {
 		part1(input)
