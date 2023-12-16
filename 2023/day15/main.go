7d3b6086a865f3752ec71e4951ac7c1cbd70643a
Alex Rios <alexrios.dev@gmail.com>
2023-12-16 05:14:01 -0600

    complete day 15 part 2

--- 2023/day15/main.go
+++ 2023/day15/main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"advent-of-code/utils"
 	"fmt"
+	"strings"
+	"unicode"
+
+	"github.com/emirpasic/gods/maps/linkedhashmap"
 )
 
 func main() {
 	lines, _ := utils.ReadLines("input.txt")
 	fmt.Println("2023 Day 15 Solution")
 	fmt.Printf("Part 1: %v\n", part1(lines))
+	fmt.Printf("Part 2: %v\n", part2(lines))
 }
 
 func part1(lines []string) int {
@@ -28,6 +33,54 @@ func part1(lines []string) int {
 	return result
 }
 
+func part2(lines []string) int {
+	boxes := make([]linkedhashmap.Map, 256)
+	for i := range boxes {
+		boxes[i] = *linkedhashmap.New()
+	}
+	for _, line := range lines {
+		sb := strings.Builder{}
+		val := 0
+		for _, c := range line {
+			if unicode.IsLetter(c) {
+				sb.WriteRune(c)
+				val = applyHash(val, c)
+				continue
+			}
+			if c == '-' {
+				label := sb.String()
+				boxes[val].Remove(label)
+				sb.Reset()
+				val = 0
+				continue
+			}
+			if c == '=' || c == ',' {
+				continue
+			}
+			label := sb.String()
+			(boxes)[val].Put(label, int(c-'0'))
+			val = 0
+			sb.Reset()
+		}
+	}
+	result := 0
+	for i, box := range boxes {
+		if !box.Empty() {
+			iter := box.Iterator()
+			slot := 1
+			for iter.Next() {
+				result += applyFocusingPower(i, slot, iter.Value().(int))
+				slot++
+			}
+		}
+	}
+	return result
+}
+
 func applyHash(val int, c rune) int {
 	return ((val + int(c)) * 17) % 256
+}
+
+func applyFocusingPower(box, slot, lens int) int {
+	return (1 + box) * slot * lens
 }
