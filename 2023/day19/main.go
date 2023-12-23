853bbe01d623eb9a07f88cd8a3b82e76a25750c9
Alex Rios <alexrios.dev@gmail.com>
2023-12-23 01:27:43 -0600

    complete day 19 part 2

--- 2023/day19/main.go
+++ 2023/day19/main.go
@@ -8,10 +8,15 @@ import (
 	"unicode"
 )
 
+type pair struct {
+	low, high int
+}
+
 func main() {
 	input, _ := os.ReadFile("input.txt")
 	fmt.Println("2023 Day 19 Solution")
 	fmt.Printf("Part 1: %v\n", part1(input))
+	fmt.Printf("Part 2: %v\n", part2(input))
 }
 
 func part1(input []byte) int {
@@ -44,6 +49,23 @@ func part1(input []byte) int {
 	return result
 }
 
+func part2(input []byte) int {
+	lines := strings.Split(strings.TrimSpace(string(input)), "\n\n")
+	part := map[rune]pair{
+		'x': {1, 4000},
+		'm': {1, 4000},
+		'a': {1, 4000},
+		's': {1, 4000},
+	}
+	workflows := map[string][]string{}
+	workflowsReplacer := strings.NewReplacer("{", " ", ",", " ", "}", "")
+	for _, s := range strings.Split(lines[0], "\n") {
+		split := strings.Fields(workflowsReplacer.Replace(s))
+		workflows[split[0]] = split[1:]
+	}
+	return getCombinations(&part, &workflows, "in")
+}
+
 func isAccepted(part *map[rune]int, workflows *map[string][]string, rule string) bool {
 	if rule == "A" {
 		return true
@@ -54,14 +76,10 @@ func isAccepted(part *map[rune]int, workflows *map[string][]string, rule string)
 			if len(split) == 2 && work[1] == '<' {
 				if (*part)[rune(work[0])] < runesToInt([]rune(work[2:])) {
 					return isAccepted(part, workflows, split[1])
-				} else {
-					continue
 				}
 			} else if len(split) == 2 && work[1] == '>' {
 				if (*part)[rune(work[0])] > runesToInt([]rune(work[2:])) {
 					return isAccepted(part, workflows, split[1])
-				} else {
-					continue
 				}
 			} else {
 				return isAccepted(part, workflows, work)
@@ -69,6 +87,47 @@ func isAccepted(part *map[rune]int, workflows *map[string][]string, rule string)
 		}
 	}
 	return false
+}
+
+func getCombinations(part *map[rune]pair, workflows *map[string][]string, rule string) int {
+	if rule == "A" {
+		prod := 1
+		for _, r := range *part {
+			prod *= r.high - r.low + 1
+		}
+		return prod
+	}
+	if rule != "R" {
+		result := 0
+		for _, work := range (*workflows)[rule] {
+			split := strings.Split(work, ":")
+			if len(split) != 2 {
+				result += getCombinations(part, workflows, work)
+				continue
+			}
+			r, num := (*part)[rune(work[0])], runesToInt([]rune(work[2:]))
+			t, f, newPart := pair{}, pair{}, map[rune]pair{}
+			for k, v := range *part {
+				newPart[k] = v
+			}
+			if work[1] == '<' {
+				t.low, t.high = r.low, num-1
+				f.low, f.high = num, r.high
+			} else if work[1] == '>' {
+				t.low, t.high = num+1, r.high
+				f.low, f.high = r.low, num
+			}
+			if t.low <= t.high {
+				newPart[rune(work[0])] = t
+				result += getCombinations(&newPart, workflows, split[1])
+			}
+			if f.low <= f.high {
+				(*part)[rune(work[0])] = f
+			}
+		}
+		return result
+	}
+	return 0
 }
 
 func runesToInt(runes []rune) int {
--- 2023/day19/main_test.go
+++ 2023/day19/main_test.go
@@ -23,9 +23,34 @@ func TestPart1(t *testing.T) {
 	}
 }
 
+func TestPart2(t *testing.T) {
+	input1, _ := os.ReadFile("input_test.txt")
+	input2, _ := os.ReadFile("input.txt")
+	tests := []struct {
+		input    []byte
+		expected int
+	}{
+		{input1, 167409079868000},
+		{input2, 113550238315130},
+	}
+	for _, test := range tests {
+		result := part2(test.input)
+		if result != test.expected {
+			t.Errorf("Result %v not equal to expected %v", result, test.expected)
+		}
+	}
+}
+
 func BenchmarkPart1(b *testing.B) {
 	input, _ := os.ReadFile("input.txt")
 	for n := 0; n < b.N; n++ {
 		part1(input)
+	}
+}
+
+func BenchmarkPart2(b *testing.B) {
+	input, _ := os.ReadFile("input.txt")
+	for n := 0; n < b.N; n++ {
+		part2(input)
 	}
 }
