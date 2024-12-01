3fe825b2a2f0af3f3fe395753879c651fa370d7e
Alex Rios <alexrios.dev@gmail.com>
2024-12-01 10:40:28 -0600

    complete 2024 day 01 part 2

--- 2024/day01/main.go
+++ 2024/day01/main.go
@@ -12,6 +12,7 @@ func main() {
 	lines, _ := utils.ReadLines("input.txt")
 	fmt.Println("2024 Day 01 Solution")
 	fmt.Printf("Part 1: %v\n", part1(lines))
+	fmt.Printf("Part 2: %v\n", part2(lines))
 }
 
 func part1(lines []string) int {
@@ -28,6 +29,27 @@ func part1(lines []string) int {
 		sum += int(getAbs(right[i] - left[i]))
 	}
 	return sum
+}
+
+func part2(lines []string) int {
+	left, right := []int{}, []int{}
+	for _, line := range lines {
+		split := strings.Fields(line)
+		left = append(left, strToInt(split[0]))
+		right = append(right, strToInt(split[1]))
+	}
+	freq := map[int]int{}
+	for _, num := range left {
+		freq[num] = 0
+	}
+	for _, num := range right {
+		freq[num]++
+	}
+	score := 0
+	for _, num := range left {
+		score += num * freq[num]
+	}
+	return score
 }
 
 func strToInt(s string) int {
