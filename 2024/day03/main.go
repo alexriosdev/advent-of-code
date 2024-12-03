d01a7ecf91154ee2612ac826c7d7ef99eb2e83c7
Alex Rios <alexrios.dev@gmail.com>
2024-12-03 02:57:30 -0600

    complete 2024 day 03 part 2

--- 2024/day03/main.go
+++ 2024/day03/main.go
@@ -12,18 +12,35 @@ func main() {
 	lines, _ := utils.ReadLines("input.txt")
 	fmt.Println("2024 Day 03 Solution")
 	fmt.Printf("Part 1: %v\n", part1(lines))
+	fmt.Printf("Part 2: %v\n", part2(lines))
 }
 
 func part1(lines []string) int {
-	result := 0
 	pattern := regexp.MustCompile(`mul\(\d+,\d+\)`)
 	replacer := strings.NewReplacer("mul(", "", ",", " ", ")", "")
-	for _, line := range lines {
-		matches := pattern.FindAllString(line, -1)
-		for _, match := range matches {
-			split := strings.Fields(replacer.Replace(match))
-			result += strToInt(split[0]) * strToInt(split[1])
-		}
+	result := 0
+	for _, match := range pattern.FindAllString(strings.Join(lines, ""), -1) {
+		split := strings.Fields(replacer.Replace(match))
+		result += strToInt(split[0]) * strToInt(split[1])
+	}
+	return result
+}
+
+func part2(lines []string) int {
+	pattern := regexp.MustCompile(`mul\(\d+,\d+\)`)
+	replacer := strings.NewReplacer("mul(", "", ",", " ", ")", "")
+	enabledPattern := regexp.MustCompile(`(do\(\))(.*?)(don't\(\)|$)`)
+	firstPattern := regexp.MustCompile(`(mul\(\d+,\d+\))(.*?)(do\(\)|don't\(\))`)
+	join := strings.Join(lines, "")
+	first := firstPattern.FindString(join)
+	matches := pattern.FindAllString(first, -1)
+	for _, enabled := range enabledPattern.FindAllString(join, -1) {
+		matches = append(matches, pattern.FindAllString(enabled, -1)...)
+	}
+	result := 0
+	for _, match := range matches {
+		split := strings.Fields(replacer.Replace(match))
+		result += strToInt(split[0]) * strToInt(split[1])
 	}
 	return result
 }
--- 2024/day03/main_test.go
+++ 2024/day03/main_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestPart1(t *testing.T) {
-	lines1, _ := utils.ReadLines("input_test.txt")
+	lines1, _ := utils.ReadLines("input_test1.txt")
 	lines2, _ := utils.ReadLines("input.txt")
 	tests := []struct {
 		input    []string
@@ -23,9 +23,34 @@ func TestPart1(t *testing.T) {
 	}
 }
 
+func TestPart2(t *testing.T) {
+	lines1, _ := utils.ReadLines("input_test2.txt")
+	lines2, _ := utils.ReadLines("input.txt")
+	tests := []struct {
+		input    []string
+		expected int
+	}{
+		{lines1, 48},
+		{lines2, 82868252},
+	}
+	for _, test := range tests {
+		result := part2(test.input)
+		if result != test.expected {
+			t.Errorf("Result %v not equal to expected %v", result, test.expected)
+		}
+	}
+}
+
 func BenchmarkPart1(b *testing.B) {
 	lines, _ := utils.ReadLines("input.txt")
 	for n := 0; n < b.N; n++ {
 		part1(lines)
+	}
+}
+
+func BenchmarkPart2(b *testing.B) {
+	lines, _ := utils.ReadLines("input.txt")
+	for n := 0; n < b.N; n++ {
+		part2(lines)
 	}
 }
