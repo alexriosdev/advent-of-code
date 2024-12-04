d3ff2e9648ce4ce0faf8cd046331e7ff29377492
Alex Rios <alexrios.dev@gmail.com>
2024-12-04 04:26:35 -0600

    complete 2024 day 04 part 2

--- 2024/day04/main.go
+++ 2024/day04/main.go
@@ -9,6 +9,7 @@ func main() {
 	lines, _ := utils.ReadLines("input.txt")
 	fmt.Println("2024 Day 04 Solution")
 	fmt.Printf("Part 1: %v\n", part1(lines))
+	fmt.Printf("Part 2: %v\n", part2(lines))
 }
 
 func part1(lines []string) int {
@@ -18,7 +19,7 @@ func part1(lines []string) int {
 	for i := range grid {
 		for j := range grid[0] {
 			for _, dir := range dirs {
-				if isValid(grid, i, j, dir) {
+				if isValidPattern("XMAS", grid, i, j, dir) {
 					count++
 				}
 			}
@@ -27,8 +28,25 @@ func part1(lines []string) int {
 	return count
 }
 
-func isValid(grid [][]rune, i, j int, dir []int) bool {
-	for _, c := range "XMAS" {
+func part2(lines []string) int {
+	grid := convertToGrid(lines)
+	count := 0
+	for i := range grid {
+		for j := range grid[0] {
+			d1 := isValidPattern("MAS", grid, i, j, []int{1, 1})
+			d2 := isValidPattern("MAS", grid, i+2, j+2, []int{-1, -1})
+			d3 := isValidPattern("MAS", grid, i+2, j, []int{-1, 1})
+			d4 := isValidPattern("MAS", grid, i, j+2, []int{1, -1})
+			if (d1 || d2) && (d3 || d4) {
+				count++
+			}
+		}
+	}
+	return count
+}
+
+func isValidPattern(s string, grid [][]rune, i, j int, dir []int) bool {
+	for _, c := range s {
 		if i < 0 || i >= len(grid) || j < 0 || j >= len(grid[0]) || c != grid[i][j] {
 			return false
 		}
