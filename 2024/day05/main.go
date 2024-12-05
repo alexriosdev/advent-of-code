56ca25ef1ea78ca64fd40652d4c60052a62aaff2
Alex Rios <alexrios.dev@gmail.com>
2024-12-05 12:18:36 -0600

    complete 2024 day 05 part 2

--- 2024/day05/main.go
+++ 2024/day05/main.go
@@ -4,6 +4,7 @@ import (
 	"advent-of-code/utils"
 	"fmt"
 	"os"
+	"slices"
 	"strings"
 )
 
@@ -11,34 +12,22 @@ func main() {
 	input, _ := os.ReadFile("input.txt")
 	fmt.Println("2024 Day 05 Solution")
 	fmt.Printf("Part 1: %v\n", part1(input))
+	fmt.Printf("Part 2: %v\n", part2(input))
 }
 
 func part1(input []byte) int {
 	sections := strings.Split(strings.TrimSpace(string(input)), "\n\n")
-	orderingMap := map[int][]int{}
-	for _, line := range strings.Fields(sections[0]) {
-		split := strings.Split(line, "|")
-		key, val := utils.StrToInt(split[0]), utils.StrToInt(split[1])
-		orderingMap[key] = append(orderingMap[key], val)
-	}
-	allPages := [][]int{}
-	for _, line := range strings.Fields(sections[1]) {
-		pages := []int{}
-		for _, s := range strings.Split(line, ",") {
-			pages = append(pages, utils.StrToInt(s))
-		}
-		allPages = append(allPages, pages)
-	}
+	orderingMap := getOrderingMap(sections[0])
+	allPages := getAllPages(sections[1])
 	nums := []int{}
 	for _, pages := range allPages {
 		count, n := 0, len(pages)
 		for i := 0; i < n-1; i++ {
 			ordering := orderingMap[pages[i]]
-			if sliceContains(ordering, pages[i+1]) {
-				count++
-			} else {
+			if !slices.Contains(ordering, pages[i+1]) {
 				break
 			}
+			count++
 		}
 		if count == n-1 {
 			nums = append(nums, pages[n/2])
@@ -51,11 +40,70 @@ func part1(input []byte) int {
 	return result
 }
 
-func sliceContains(nums []int, target int) bool {
-	for _, num := range nums {
-		if num == target {
-			return true
+func part2(input []byte) int {
+	sections := strings.Split(strings.TrimSpace(string(input)), "\n\n")
+	orderingMap := getOrderingMap(sections[0])
+	allPages := getAllPages(sections[1])
+	incorrectPages := [][]int{}
+	for _, pages := range allPages {
+		for i := 0; i < len(pages)-1; i++ {
+			ordering := orderingMap[pages[i]]
+			if !slices.Contains(ordering, pages[i+1]) {
+				incorrectPages = append(incorrectPages, pages)
+				break
+			}
 		}
 	}
-	return false
+	nums := []int{}
+	for _, pages := range incorrectPages {
+		isComplete := false
+		for !isComplete {
+			if isOrdered(&pages, &orderingMap) {
+				isComplete = true
+			}
+		}
+		nums = append(nums, pages[len(pages)/2])
+	}
+	result := 0
+	for _, num := range nums {
+		result += num
+	}
+	return result
+}
+
+func getOrderingMap(section string) map[int][]int {
+	orderingMap := map[int][]int{}
+	for _, line := range strings.Fields(section) {
+		split := strings.Split(line, "|")
+		key, val := utils.StrToInt(split[0]), utils.StrToInt(split[1])
+		orderingMap[key] = append(orderingMap[key], val)
+	}
+	return orderingMap
+}
+
+func getAllPages(section string) [][]int {
+	allPages := [][]int{}
+	for _, line := range strings.Fields(section) {
+		pages := []int{}
+		for _, s := range strings.Split(line, ",") {
+			pages = append(pages, utils.StrToInt(s))
+		}
+		allPages = append(allPages, pages)
+	}
+	return allPages
+}
+
+func isOrdered(pages *[]int, orderingMap *map[int][]int) bool {
+	for start, ordering := range *orderingMap {
+		for _, end := range ordering {
+			i, j := slices.Index(*pages, start), slices.Index(*pages, end)
+			if i != -1 && j != -1 && i >= j {
+				shift := append((*pages)[:j], (*pages)[j+1:]...)
+				shift = append(shift, end)
+				*pages = shift
+				return false
+			}
+		}
+	}
+	return true
 }
