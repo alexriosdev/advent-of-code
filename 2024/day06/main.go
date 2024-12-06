914ae6f79e87b323bb61140970ebe392b87c4291
Alex Rios <alexrios.dev@gmail.com>
2024-12-06 17:06:20 -0600

    complete 2024 day 06 part 2

--- 2024/day06/main.go
+++ 2024/day06/main.go
@@ -9,26 +9,29 @@ func main() {
 	lines, _ := utils.ReadLines("input.txt")
 	fmt.Println("2024 Day 06 Solution")
 	fmt.Printf("Part 1: %v\n", part1(lines))
+	fmt.Printf("Part 2: %v\n", part2(lines))
 }
 
 func part1(lines []string) int {
 	grid := convertToGrid(lines)
 	start := getStart(grid)
-	visited := map[coordinate]bool{}
-	dirs := [][]int{{-1, 0}, {0, 1}, {1, 0}, {0, -1}}
-	i := 0
-	for 0 <= start.x && start.x < len(grid) && 0 <= start.y && start.y < len(grid[0]) {
-		if grid[start.x][start.y] != '#' {
-			visited[start] = true
-			start.x += dirs[i%4][0]
-			start.y += dirs[i%4][1]
-		} else {
-			start.x -= dirs[i%4][0]
-			start.y -= dirs[i%4][1]
-			i++
-		}
-	}
+	visited := getVisited(grid, start)
 	return len(visited)
+}
+
+func part2(lines []string) int {
+	grid := convertToGrid(lines)
+	start := getStart(grid)
+	visited := getVisited(grid, start)
+	result := 0
+	for cell := range visited {
+		grid[cell.x][cell.y] = '#'
+		if isCycle(grid, start) {
+			result++
+		}
+		grid[cell.x][cell.y] = '.'
+	}
+	return result
 }
 
 type coordinate struct {
@@ -52,4 +55,47 @@ func getStart(grid [][]rune) coordinate {
 		}
 	}
 	return coordinate{-1, -1}
+}
+
+func getVisited(grid [][]rune, start coordinate) map[coordinate]bool {
+	visited := map[coordinate]bool{}
+	dirs := [][]int{{-1, 0}, {0, 1}, {1, 0}, {0, -1}}
+	i := 0
+	for isRange(grid, start.x, start.y) {
+		visited[start] = true
+		nextX := start.x + dirs[i%4][0]
+		nextY := start.y + dirs[i%4][1]
+		if isRange(grid, nextX, nextY) && grid[nextX][nextY] == '#' {
+			i++
+		} else {
+			start.x = nextX
+			start.y = nextY
+		}
+	}
+	return visited
+}
+
+func isCycle(grid [][]rune, start coordinate) bool {
+	visited := map[coordinate]int{}
+	dirs := [][]int{{-1, 0}, {0, 1}, {1, 0}, {0, -1}}
+	i := 0
+	for isRange(grid, start.x, start.y) {
+		if visited[start] > len(dirs) {
+			return true
+		}
+		visited[start]++
+		nextX := start.x + dirs[i%4][0]
+		nextY := start.y + dirs[i%4][1]
+		if isRange(grid, nextX, nextY) && grid[nextX][nextY] == '#' {
+			i++
+		} else {
+			start.x = nextX
+			start.y = nextY
+		}
+	}
+	return false
+}
+
+func isRange(grid [][]rune, i, j int) bool {
+	return 0 <= i && i < len(grid) && 0 <= j && j < len(grid[0])
 }
