04c17a25b1f63c752a7e89bc57f863c2dbae091d
Alex Rios <alexrios.dev@gmail.com>
2024-12-10 05:16:23 -0600

    complete 2024 day 10 part 2

--- 2024/day10/main.go
+++ 2024/day10/main.go
@@ -12,6 +12,7 @@ func main() {
 	lines, _ := utils.ReadLines("input.txt")
 	fmt.Println("2024 Day 10 Solution")
 	fmt.Printf("Part 1: %v\n", part1(lines))
+	fmt.Printf("Part 2: %v\n", part2(lines))
 }
 
 func part1(lines []string) int {
@@ -20,7 +21,22 @@ func part1(lines []string) int {
 	for i, row := range grid {
 		for j, c := range row {
 			if c == '0' {
-				result += bfs(grid, coordinate{i, j})
+				score, _ := bfs(grid, coordinate{i, j})
+				result += score
+			}
+		}
+	}
+	return result
+}
+
+func part2(lines []string) int {
+	grid := convertToGrid(lines)
+	result := 0
+	for i, row := range grid {
+		for j, c := range row {
+			if c == '0' {
+				_, rating := bfs(grid, coordinate{i, j})
+				result += rating
 			}
 		}
 	}
@@ -31,18 +47,19 @@ type coordinate struct {
 	x, y int
 }
 
-func bfs(grid [][]rune, start coordinate) int {
+func bfs(grid [][]rune, start coordinate) (int, int) {
 	dirs := [][]int{{-1, 0}, {0, 1}, {1, 0}, {0, -1}}
 	queue := linkedlistqueue.New()
 	queue.Enqueue(start)
-	set := hashset.New()
+	score, rating := hashset.New(), 0
 	for !queue.Empty() {
 		size := queue.Size()
 		for i := 0; i < size; i++ {
 			val, _ := queue.Dequeue()
 			curr := val.(coordinate)
 			if grid[curr.x][curr.y] == '9' {
-				set.Add(coordinate{curr.x, curr.y})
+				score.Add(coordinate{curr.x, curr.y})
+				rating++
 			}
 			for _, dir := range dirs {
 				nextX := curr.x + dir[0]
@@ -54,7 +71,7 @@ func bfs(grid [][]rune, start coordinate) int {
 			}
 		}
 	}
-	return set.Size()
+	return score.Size(), rating
 }
 
 func convertToGrid(lines []string) [][]rune {
