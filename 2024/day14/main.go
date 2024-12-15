c4f3540be96f70635304ef7fe1499b80adce8615
Alex Rios <alexrios.dev@gmail.com>
2024-12-15 15:21:42 -0600

    complete 2024 day 14 part 2

--- 2024/day14/main.go
+++ 2024/day14/main.go
@@ -10,6 +10,7 @@ func main() {
 	lines, _ := utils.ReadLines("input.txt")
 	fmt.Println("2024 Day 14 Solution")
 	fmt.Printf("Part 1: %v\n", part1(lines, 103, 101))
+	fmt.Printf("Part 2: %v\n", part2(lines, 103, 101, true))
 }
 
 func part1(lines []string, m, n int) int {
@@ -46,6 +47,34 @@ func part1(lines []string, m, n int) int {
 	return result
 }
 
+func part2(lines []string, m, n int, useDisplay bool) int {
+	robots := getRobots(lines)
+	grid := buildGrid(m, n)
+	for sec := 1; sec <= m*n; sec++ {
+		for i := range robots {
+			robots[i].p.x = (robots[i].p.x + robots[i].v.x) % n
+			robots[i].p.y = (robots[i].p.y + robots[i].v.y) % m
+			if robots[i].p.x < 0 {
+				abs := utils.Abs(robots[i].p.x)
+				robots[i].p.x = n - abs
+			}
+			if robots[i].p.y < 0 {
+				abs := utils.Abs(robots[i].p.y)
+				robots[i].p.y = m - abs
+			}
+			grid[robots[i].p.y][robots[i].p.x] = '#'
+		}
+		if useDisplay {
+			displayGrid(&grid, sec)
+		}
+		if findOutline(&grid) {
+			return sec
+		}
+		clearGrid(&grid)
+	}
+	return -1
+}
+
 type coordinate struct {
 	x, y int
 }
@@ -66,4 +95,104 @@ func getRobots(lines []string) []robot {
 		robots = append(robots, robot)
 	}
 	return robots
+}
+
+func buildGrid(m, n int) [][]rune {
+	grid := make([][]rune, m)
+	for i := range grid {
+		grid[i] = make([]rune, n)
+		for j := range grid[i] {
+			grid[i][j] = '.'
+		}
+	}
+	return grid
+}
+
+func displayGrid(grid *[][]rune, second int) {
+	fmt.Println(second)
+	for _, row := range *grid {
+		fmt.Println(string(row))
+	}
+	fmt.Println()
+	// milliseconds := 50
+	// time.Sleep(time.Duration(milliseconds) * time.Millisecond)
+}
+
+func clearGrid(grid *[][]rune) {
+	for i := range *grid {
+		for j := range (*grid)[i] {
+			(*grid)[i][j] = '.'
+		}
+	}
+}
+
+func findOutline(grid *[][]rune) bool {
+	outlineSize := 30
+	top, bottom := 0, 0
+	left, right := 0, 0
+	for i := 0; i < len(*grid)-outlineSize; i++ {
+		if top >= outlineSize {
+			break
+		}
+		for j := 0; j < len((*grid)[0])-outlineSize; j++ {
+			if (*grid)[i][j] != '#' {
+				top = 0
+				continue
+			} else {
+				top++
+			}
+			if top >= outlineSize {
+				for k := i; k < len(*grid)-outlineSize; k++ {
+					if bottom >= outlineSize {
+						break
+					}
+					for l := 0; l < len((*grid)[0])-outlineSize; l++ {
+						if (*grid)[k][l] != '#' {
+							bottom = 0
+							continue
+						} else {
+							bottom++
+						}
+						if bottom >= outlineSize {
+							break
+						}
+					}
+				}
+				break
+			}
+		}
+	}
+	for i := 0; i < len((*grid)[0])-outlineSize; i++ {
+		if left >= outlineSize {
+			break
+		}
+		for j := 0; j < len(*grid)-outlineSize; j++ {
+			if (*grid)[j][i] != '#' {
+				left = 0
+				continue
+			} else {
+				left++
+			}
+			if left >= outlineSize {
+				for k := i; k < len((*grid)[0])-outlineSize; k++ {
+					if right >= outlineSize {
+						break
+					}
+					for l := 0; l < len(*grid)-outlineSize; l++ {
+						if (*grid)[l][k] != '#' {
+							right = 0
+							continue
+						} else {
+							right++
+						}
+						if right >= outlineSize {
+							break
+						}
+					}
+				}
+				break
+			}
+		}
+	}
+	return top == outlineSize && top == bottom && left == outlineSize && left == right
 }
--- 2024/day14/main_test.go
+++ 2024/day14/main_test.go
@@ -23,9 +23,33 @@ func TestPart1(t *testing.T) {
 	}
 }
 
+func TestPart2(t *testing.T) {
+	lines2, _ := utils.ReadLines("input.txt")
+	tests := []struct {
+		input          []string
+		useDisplay     bool
+		m, n, expected int
+	}{
+		{lines2, false, 103, 101, 7569},
+	}
+	for _, test := range tests {
+		result := part2(test.input, test.m, test.n, test.useDisplay)
+		if result != test.expected {
+			t.Errorf("Result %v not equal to expected %v", result, test.expected)
+		}
+	}
+}
+
 func BenchmarkPart1(b *testing.B) {
 	lines, _ := utils.ReadLines("input.txt")
 	for n := 0; n < b.N; n++ {
 		part1(lines, 103, 101)
+	}
+}
+
+func BenchmarkPart2(b *testing.B) {
+	lines, _ := utils.ReadLines("input.txt")
+	for n := 0; n < b.N; n++ {
+		part2(lines, 103, 101, false)
 	}
 }
