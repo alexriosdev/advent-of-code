204587ee187ac6a5bc1d41b8b5875eba71640fe1
Alex Rios <alexrios.dev@gmail.com>
2025-12-01 11:56:02 -0600

    feat(2025): complete day01 part2

--- 2025/day01/main.go
+++ 2025/day01/main.go
@@ -6,9 +6,11 @@ import (
 )
 
 func main() {
-	lines, _ := utils.ReadLines("2025/day01/input1.txt")
+	lines1, _ := utils.ReadLines("2025/day01/input1.txt")
+	lines2, _ := utils.ReadLines("2025/day01/input1.txt")
 	fmt.Println("2025 Day 01 Solution")
-	fmt.Printf("Part 1: %v\n", part1(lines))
+	fmt.Printf("Part 1: %v\n", part1(lines1))
+	fmt.Printf("Part 2: %v\n", part2(lines2))
 }
 
 func part1(lines []string) int {
@@ -19,6 +21,17 @@ func part1(lines []string) int {
 		dir, dist := rune(line[0]), utils.StrToInt(line[1:])
 		dial.Rotate(dir, dist)
 		dial.CountZeroes()
+	}
+	return dial.Zeroes
+}
+
+func part2(lines []string) int {
+	dial := &Dial{}
+	dial.Init(99)
+	dial.Rotate('R', 50)
+	for _, line := range lines {
+		dir, dist := rune(line[0]), utils.StrToInt(line[1:])
+		dial.RotateWithCount(dir, dist)
 	}
 	return dial.Zeroes
 }
@@ -68,6 +81,17 @@ func (d *Dial) Rotate(dir rune, dist int) {
 		} else {
 			d.Head = d.Head.Prev
 		}
+	}
+}
+
+func (d *Dial) RotateWithCount(dir rune, dist int) {
+	for i := 0; i < dist; i++ {
+		if dir == 'R' {
+			d.Head = d.Head.Next
+		} else {
+			d.Head = d.Head.Prev
+		}
+		d.CountZeroes()
 	}
 }
 
--- 2025/day01/main_test.go
+++ 2025/day01/main_test.go
@@ -23,9 +23,34 @@ func TestPart1(t *testing.T) {
 	}
 }
 
+func TestPart2(t *testing.T) {
+	lines1, _ := utils.ReadLines("input_test1.txt")
+	lines2, _ := utils.ReadLines("input1.txt")
+	tests := []struct {
+		input    []string
+		expected int
+	}{
+		{lines1, 6},
+		{lines2, 6268},
+	}
+	for _, test := range tests {
+		result := part2(test.input)
+		if result != test.expected {
+			t.Errorf("Result %v not equal to expected %v", result, test.expected)
+		}
+	}
+}
+
 func BenchmarkPart1(b *testing.B) {
 	lines, _ := utils.ReadLines("input1.txt")
 	for n := 0; n < b.N; n++ {
 		part1(lines)
+	}
+}
+
+func BenchmarkPart2(b *testing.B) {
+	lines, _ := utils.ReadLines("input2.txt")
+	for n := 0; n < b.N; n++ {
+		part2(lines)
 	}
 }
