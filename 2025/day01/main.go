a8940f9ccaa01fc08d24419340fab066c52ef25f
Alex Rios <alexrios.dev@gmail.com>
2025-12-01 11:42:14 -0600

    feat(2025): complete day01 part1

---
+++ 2025/day01/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"advent-of-code/utils"
+	"fmt"
+)
+
+func main() {
+	lines, _ := utils.ReadLines("2025/day01/input1.txt")
+	fmt.Println("2025 Day 01 Solution")
+	fmt.Printf("Part 1: %v\n", part1(lines))
+}
+
+func part1(lines []string) int {
+	dial := &Dial{}
+	dial.Init(99)
+	dial.Rotate('R', 50)
+	for _, line := range lines {
+		dir, dist := rune(line[0]), utils.StrToInt(line[1:])
+		dial.Rotate(dir, dist)
+		dial.CountZeroes()
+	}
+	return dial.Zeroes
+}
+
+type Node struct {
+	Val        int
+	Prev, Next *Node
+}
+
+type Dial struct {
+	Head        *Node
+	Len, Zeroes int
+}
+
+func (d *Dial) Init(size int) *Dial {
+	for i := size; i >= 0; i-- {
+		d.Insert(i)
+	}
+	return d
+}
+
+func (d *Dial) Insert(val int) {
+	curr := &Node{Val: val}
+	d.Len++
+
+	if d.Head == nil {
+		curr.Next = curr
+		curr.Prev = curr
+		d.Head = curr
+		return
+	}
+
+	prev := d.Head.Prev
+	curr.Next = d.Head
+	curr.Prev = prev
+
+	d.Head.Prev = curr
+	prev.Next = curr
+
+	d.Head = curr
+}
+
+func (d *Dial) Rotate(dir rune, dist int) {
+	for i := 0; i < dist; i++ {
+		if dir == 'R' {
+			d.Head = d.Head.Next
+		} else {
+			d.Head = d.Head.Prev
+		}
+	}
+}
+
+func (d *Dial) CountZeroes() {
+	if d.Head.Val == 0 {
+		d.Zeroes++
+	}
+}
---
+++ 2025/day01/main_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"advent-of-code/utils"
+	"testing"
+)
+
+func TestPart1(t *testing.T) {
+	lines1, _ := utils.ReadLines("input_test1.txt")
+	lines2, _ := utils.ReadLines("input1.txt")
+	tests := []struct {
+		input    []string
+		expected int
+	}{
+		{lines1, 3},
+		{lines2, 1086},
+	}
+	for _, test := range tests {
+		result := part1(test.input)
+		if result != test.expected {
+			t.Errorf("Result %v not equal to expected %v", result, test.expected)
+		}
+	}
+}
+
+func BenchmarkPart1(b *testing.B) {
+	lines, _ := utils.ReadLines("input1.txt")
+	for n := 0; n < b.N; n++ {
+		part1(lines)
+	}
+}
