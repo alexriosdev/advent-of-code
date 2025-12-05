c86ced2bc0cc3f4d24030dedad441b564848a247
Alex Rios <alexrios.dev@gmail.com>
2025-12-04 23:55:23 -0600

    feat: update template

--- template/main.go
+++ template/main.go
@@ -6,11 +6,16 @@ import (
 )
 
 func main() {
-	lines, _ := utils.ReadLines("input.txt")
+	lines, _ := utils.ReadLines("202#/day##/input.txt")
 	fmt.Println("202# Day ## Solution")
 	fmt.Printf("Part 1: %v\n", part1(lines))
+	fmt.Printf("Part 2: %v\n", part2(lines))
 }
 
 func part1(lines []string) int {
+	return len(lines)
+}
+
+func part2(lines []string) int {
 	return len(lines)
 }
