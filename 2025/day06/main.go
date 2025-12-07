a4d719c1fa312abc36272af5c646608bd67e1d80
Alex Rios <alexrios.dev@gmail.com>
2025-12-07 00:38:48 -0600

    feat(2025): complete day06 part2

--- 2025/day06/main.go
+++ 2025/day06/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"advent-of-code/utils"
 	"fmt"
+	"slices"
 	"strings"
+	"unicode"
 )
 
 func main() {
@@ -17,7 +19,8 @@ func part1(lines []string) int {
 	problems := [][]int{}
 	for i := 0; i < len(lines)-1; i++ {
 		nums := []int{}
-		for _, s := range strings.Fields(lines[i]) {
+		replacer := strings.NewReplacer("x", "")
+		for _, s := range strings.Fields(replacer.Replace(lines[i])) {
 			nums = append(nums, utils.StrToInt(s))
 		}
 		problems = append(problems, nums)
@@ -43,5 +46,52 @@ func part1(lines []string) int {
 }
 
 func part2(lines []string) int {
-	return len(lines)
+	grid := convertToGrid(lines)
+	rows, cols := len(grid), len(grid[0])
+	isMult, isSum := false, false
+	mult, sum := 1, 0
+	result := 0
+	for c := cols - 1; c >= 0; c-- {
+		runes := []rune{}
+		for r := rows - 1; r >= 0; r-- {
+			val := grid[r][c]
+			if unicode.IsNumber(val) {
+				runes = append(runes, val)
+			}
+			if val == '*' {
+				isMult = true
+				isSum = false
+			}
+			if val == '+' {
+				isSum = true
+				isMult = false
+			}
+		}
+		if len(runes) == 0 {
+			continue
+		}
+		slices.Reverse(runes)
+		num := utils.StrToInt(string(runes))
+		mult *= num
+		sum += num
+		if isMult && mult > 1 {
+			result += mult
+			mult, sum = 1, 0
+			isMult = false
+		}
+		if isSum {
+			result += sum
+			mult, sum = 1, 0
+			isSum = false
+		}
+	}
+	return result
+}
+
+func convertToGrid(lines []string) [][]rune {
+	grid := [][]rune{}
+	for _, line := range lines {
+		grid = append(grid, []rune(line))
+	}
+	return grid
 }
