d19ced3a791af282e4a75f00234967328e4192ea
Alex Rios <alexrios.dev@gmail.com>
2025-12-09 14:11:00 -0600

    feat(2025): complete day09 part2

--- 2025/day09/main.go
+++ 2025/day09/main.go
@@ -24,15 +24,29 @@ func part1(lines []string) int {
 }
 
 func part2(lines []string) int {
-	return len(lines)
+	positions := Positions{}
+	for _, line := range lines {
+		split := strings.Split(line, ",")
+		pos := Pos{utils.StrToInt(split[0]), utils.StrToInt(split[1])}
+		positions.Add(pos)
+	}
+	return positions.MaxRectangleInBounds()
 }
 
 type Pos struct {
 	X, Y int
 }
 
-func (a *Pos) Rectangle(b Pos) int {
+func Rectangle(a, b Pos) int {
 	return (utils.Abs(a.X-b.X) + 1) * (utils.Abs(a.Y-b.Y) + 1)
+}
+
+func IsOverlap(a, b, c, d Pos) bool {
+	minX, maxX := min(a.X, b.X), max(a.X, b.X)
+	minY, maxY := min(a.Y, b.Y), max(a.Y, b.Y)
+	xOverlap := maxX > min(c.X, d.X) && minX < max(c.X, d.X)
+	yOverlap := maxY > min(c.Y, d.Y) && minY < max(c.Y, d.Y)
+	return xOverlap && yOverlap
 }
 
 type Positions []Pos
@@ -43,17 +57,35 @@ func (p *Positions) Add(pos Pos) {
 
 func (p *Positions) MaxRectangle() int {
 	maxRect := 0
-	visited := map[Pos]bool{}
 	for i := 0; i < len(*p); i++ {
 		for j := i + 1; j < len(*p); j++ {
-			PosA := (*p)[i]
-			PosB := (*p)[j]
-			if visited[PosB] {
-				continue
-			}
-			visited[PosA] = true
-			maxRect = max(maxRect, PosA.Rectangle(PosB))
+			a, b := (*p)[i], (*p)[j]
+			maxRect = max(maxRect, Rectangle(a, b))
 		}
 	}
 	return maxRect
+}
+
+func (p *Positions) MaxRectangleInBounds() int {
+	maxRect := 0
+	for i := 0; i < len(*p); i++ {
+		for j := i + 1; j < len(*p); j++ {
+			a, b := (*p)[i], (*p)[j]
+			if (*p).IsInBounds(a, b) {
+				maxRect = max(maxRect, Rectangle(a, b))
+			}
+		}
+	}
+	return maxRect
+}
+
+func (p *Positions) IsInBounds(a, b Pos) bool {
+	for i := 0; i < len(*p); i++ {
+		j := (i + 1) % len(*p)
+		c, d := (*p)[i], (*p)[j]
+		if IsOverlap(a, b, c, d) {
+			return false
+		}
+	}
+	return true
 }
